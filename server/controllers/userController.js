b6a920eddb56e58c66578e325f4a5889984fd75f
mdev1987 <m.developer66@gmail.com>
2023-01-19 23:05:16 +0330

    modify addRemoveFriend method in userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -44,38 +44,32 @@ export const getUserFriends = async (req, res) => {
 export const addRemoveFriend = async (req, res) => {
     try {
         const { id, friendId } = req.params;
-        const user = await userModel.findById(id);
+        let user = await userModel.findById(id);
         const friend = await userModel.findById(friendId);
 
         if (!user || !friend) return res.status(404)
             .json({ message: 'User not found' });
 
-        if (user.friends.includes(friendId)) {            
-            user.friends = user.friends
-                .filter(fId => fId.toString() !== friendId)
-            friend.friends = friend.friends
-                .filter(fId => fId.toString() !== id)                             
-        } else if (!user.friends.includes(friendId)) {            
-            user.friends.push(friendId)
-            friend.friends.push(id);
+        if (user.friends.includes(friendId)) {
+            await user.updateOne({ $pull: { friends: friendId } });
+            await friend.updateOne({ $pull: { friends: id } })
+        } else if (!user.friends.includes(friendId)) {
+            await user.updateOne({ $push: { friends: friendId } });
+            await friend.updateOne({ $push: { friends: id } })
         }
 
-        await user.save();
-        await friend.save();
         const usr = await userModel.findById(id, {
             password: 0,
             createdAt: 0,
             updatedAt: 0
-        })
-            .populate('friends', {
-                _id: 1,
-                firstName: 1,
-                lastName: 1,
-                occupation: 1,
-                location: 1,
-                picturePath: 1
-            })
-            .lean()
+        }).populate('friends', {
+            _id: 1,
+            firstName: 1,
+            lastName: 1,
+            occupation: 1,
+            location: 1,
+            picturePath: 1
+        }).lean()
         res.status(200).json(usr);
     } catch (ex) {
         res.status(500).json({ message: ex.message })
