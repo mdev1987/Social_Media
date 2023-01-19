61bbc99ca007671ba26afff3a84cb65f398e4a27
mdev1987 <m.developer66@gmail.com>
2023-01-19 22:39:58 +0330

    define multer configuration in multerConfig.js file

--- server/routers/authRoute.js
+++ server/routers/authRoute.js
@@ -1,21 +1,10 @@
 import { Router } from 'express';
-import multer from 'multer';
+import { upload } from '../utilities/multerConfig.js';
 import { loginUser, registerUser } from '../controllers/authController.js';
 
 const router = Router();
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'public/assets')
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname)
-    }
-});
-const upload = multer({ storage })
-
 router.post('/register', upload.single('picture'), registerUser)
-
 router.post('/login', loginUser)
 
 export default router;
