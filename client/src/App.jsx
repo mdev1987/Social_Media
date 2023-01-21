4f06f4dc31737cbca2ada6640f3b9093b6e0b072
mdev1987 <m.developer66@gmail.com>
2023-01-21 04:48:17 +0330

    create themeSettings and setup dark/light theme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,28 @@
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import HomePage from "./pages/HomePage";
 import LoginPage from './pages/LoginPage'
 import ProfilePage from "./pages/ProfilePage";
+import { themeSettings } from "./theme";
 
 function App() {
+  const mode = useSelector(state => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+
   return (
     <div className="app">
-     <BrowserRouter>
-     <Routes>
-      <Route path="/" element={<LoginPage/>} />
-      <Route path="/home" element={<HomePage/>} />
-      <Route path="/profile/:userId" element={<ProfilePage/>} />
-     </Routes>
-     </BrowserRouter>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
