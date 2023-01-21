2aee17ecacd89f8d21b75a87c0df746c7f050f59
mdev1987 <m.developer66@gmail.com>
2023-01-22 01:58:24 +0330

    create AuthForm component and complete LoginPage

---
+++ client/src/consts/apiRoute.js
@@ -0,0 +1,6 @@
+export const HOST_ADDRESS = 'http://localhost:3500';
+
+// auth api
+export const LOGIN_API = `${HOST_ADDRESS}/auth/login`;
+export const REGISTER_API = `${HOST_ADDRESS}/auth/register`;
+
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import { Box, Typography, useTheme, useMediaQuery } from '@mui/material'
+import AuthForm from '../components/AuthForm';
 
 function LoginPage() {
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");  
+  const primaryLight = theme.palette.primary.light;
+  const alt = theme.palette.background.alt;
   return (
-    <div>LoginPage</div>
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={alt}
+        p="1rem 6%"
+        textAlign="center">
+        <Typography
+          fontWeight="bold"
+          fontSize="32px"
+          color="primary"
+          sx={{
+            "&:hover": {
+              color: primaryLight,
+              curosor: "pointer"
+            }
+          }}>
+          Sociopedia
+        </Typography>
+      </Box>
+
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={alt}>
+        <Typography
+          fontWeight="500"
+          variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to Socipedia, the Social Media for Sociopaths!
+        </Typography>
+        <AuthForm />
+      </Box>
+    </Box>
   )
 }
 
