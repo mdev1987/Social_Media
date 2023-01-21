622450e3446b7e56125a90dcac11faa9ce69b60d
mdev1987 <m.developer66@gmail.com>
2023-01-22 03:23:41 +0330

    Hotfix: fix verifyToken when token starts with 'Bearer '

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -5,7 +5,7 @@ export const verifyToken = async (req, res, next) => {
         let token = req.header('Authorization');
         if (!token) return res.status(403).json({ message: "Access Denied" });
         if (token.startsWith('Bearer ')) {
-            token = token.slice(0, token.length).trimLeft();
+            token = token.slice('Bearer '.length, token.length).trimLeft();
         }
         const verifyUser = jwt.verify(token, process.env.JWT_SECRET)
         if (verifyUser) {
